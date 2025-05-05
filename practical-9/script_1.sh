6b98ac71646883323aae60f595331f949f60587d
vladlen-cmd <vladlen.rebello.btech2023@sitnagpur.siu.edu.in>
2025-05-05 13:02:12 +0530

    Create script_1.sh

---
+++ practical-9/script_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Display kernel version
+echo "Kernel Version:"
+uname -r
+
+# Display current user
+echo "Current User:"
+whoami
+
+# Display system uptime
+echo "System Uptime:"
+uptime
