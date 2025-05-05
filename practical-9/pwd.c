a9234dcd562636fe63797aa46058982f335cd2d4
vladlen-cmd <vladlen.rebello.btech2023@sitnagpur.siu.edu.in>
2025-05-05 13:01:13 +0530

    Create pwd.c

---
+++ practical-9/pwd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("%s\n", cwd);
+    } else {
+        perror("getcwd");
+    }
+    return 0;
+}
